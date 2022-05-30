b3b578e09151717d3a85561fa85a1e3999336e10
Shota Narisaka <shootacean@gmail.com>
2022-05-31 08:50:37 +0900

    fix(多国通貨): タイポの修正 (USG → USD)

--- src/money.spec.ts
+++ src/money.spec.ts
@@ -44,7 +44,7 @@ test('Test Reduce Sum', () => {
 
 test('Test Reduce Money', () => {
   const bank: Bank = new Bank();
-  const result: Money = bank.reduce(Money.dollar(1), 'USG');
+  const result: Money = bank.reduce(Money.dollar(1), 'USD');
   expect(result).toEqual(Money.dollar(1));
 });
 
